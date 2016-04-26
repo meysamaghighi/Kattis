dfded99e37500bb00d56e0c3a75018e194e00423
Meysam Aghighi <meysam.aghighi@gmail.com>
2016-04-26 15:27:03 +0200

    Update a.cpp

--- Factovisors/a.cpp
+++ Factovisors/a.cpp
@@ -7,11 +7,10 @@ using namespace std;
 bool solve(int n, int m){
     if (m == 0) return false;
     if (m == 1 && n == 0) return true;
-    for (int i=2,rm=sqrt(m)+1,k,p;i<rm && m>n;i++){ // sqrt() faster than i*i<=m
-        k = 0;
-        while (m%i==0){ m/=i; k++; } // i^k|m
-        p = i;
-        while (p<=n && k>0) { k-=n/p; p*=i; } // i^kk|n!
+    for (int i=2,rm=sqrt(m)+1,k,p;i<rm && m>n;i++){
+        k=0, p=i;
+        while (m%i==0) m/=i, k++; // max k where i^k|m
+        while (p<=n && k>0) k-=n/p, p*=i; // max kk where i^kk|n! (the other way)
         if (k>0) return false;
     }
     return m <= n;    
