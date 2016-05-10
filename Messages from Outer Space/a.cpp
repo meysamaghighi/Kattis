8b97708aa8c015c600974e6ada0df910f07ea7bd
meysamaghighi <meysam.aghighi@gmail.com>
2016-05-11 01:53:32 +0200

    update

--- Messages from Outer Space/a.cpp
+++ Messages from Outer Space/a.cpp
@@ -1,80 +1,121 @@
 // Meysam Aghighi
+#include <algorithm>
 #include <iostream>
 #include <cstring>
+#include <cstdio>
 #include <vector>
+#include <queue>
 #include <map>
 using namespace std;
 
 vector<string> dic;
-map <string,int> cache;
+map <int,int> cache;
 
-void computeLPSArray(string pat, int *lps){
-    int len = 0;
-    int i = 1; 
-    lps[0] = 0; 
-    while (i < pat.size()){
-       if (pat[i] == pat[len]){
-         len++;
-         lps[i] = len;
-         i++;
-       }
-       else{       
-         if (len != 0) len = lps[len-1];
-         else lps[i] = 0, i++;
-       }
+#define iter(it,c) for (__typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+typedef vector<vector<int> > vvi;
+map<string,int> all;
+vvi res;
+string s;
+
+// Aho-Corasick from SuprDewd
+struct aho_corasick {
+    struct out_node {
+        string keyword; out_node *next;
+        out_node(string k, out_node *n) : keyword(k), next(n) { }
+    };
+    struct go_node {
+        map<char, go_node*> next;
+        out_node *out; go_node *fail;
+        go_node() { out = NULL; fail = NULL; }
+    };
+    go_node *go;
+    int N;
+    aho_corasick(vector<string> keywords) {
+        N = keywords.size();
+        go = new go_node();
+        iter(k, keywords) {
+            go_node *cur = go;
+            iter(c, *k)
+                cur = cur->next.find(*c) != cur->next.end() ? cur->next[*c] :
+                    (cur->next[*c] = new go_node());
+            cur->out = new out_node(*k, cur->out);
+        }
+        queue<go_node*> q;
+        iter(a, go->next) q.push(a->second);
+        while (!q.empty()) {
+            go_node *r = q.front(); q.pop();
+            iter(a, r->next) {
+                go_node *s = a->second;
+                q.push(s);
+                go_node *st = r->fail;
+                while (st && st->next.find(a->first) == st->next.end())
+                    st = st->fail;
+                if (!st) st = go;
+                s->fail = st->next[a->first];
+                if (s->fail) {
+                    if (!s->out) s->out = s->fail->out;
+                    else {
+                        out_node* out = s->out;
+                        while (out->next) out = out->next;
+                        out->next = s->fail->out;
+                    }
+                }
+            }
+        }
     }
-}
-
-int findFirst(string pat, string txt){
-    int M = pat.size();
-    int N = txt.size(); 
-    int * lps = new int[M];
-    int j  = 0 , i = 0;
- 
-    computeLPSArray(pat, lps);
-
-    while (i < N){
-      if (pat[j] == txt[i]) i++ , j++;
- 
-      if (j == M){
-        // cout << i-j << " ";
-        return i-j;
-        j = lps[j-1];
-      }
- 
-      else if (i < N && pat[j] != txt[i]){
-        if (j != 0) j = lps[j-1];
-        else i++;
-      }
+    vvi search(string s){
+        vvi ress(N);
+        go_node *cur = go;
+        iter(c, s) {
+            while (cur && cur->next.find(*c) == cur->next.end())
+                cur = cur->fail;
+            if (!cur) cur = go;
+            cur = cur->next[*c];
+            if (!cur) cur = go;
+            for (out_node *out = cur->out; out; out = out->next){
+                ress[all[out->keyword]].push_back(c-s.begin()-out->keyword.size()+1);
+            }
+        }
+        return ress;
     }
-    
-    return -1;
-}
+};
 
-int solve(string s){
-    // cerr << s << endl;
-    if (s.size() == 0) return 0;
-    if (cache.count(s)) return cache[s];
+int solve(int p){
+    if (s.size() == p) return 0;
+    if (cache.count(p)) return cache[p];
     int ans = 0;
     for (int i=0;i<dic.size();i++){
-        int it = findFirst(dic[i],s);
-        // cerr << dic[i] << " " << s << " : " << it << endl;
-        if (it != -1) ans = max( ans , 1 + solve(s.substr(0,it)) + solve(s.substr(it+dic[i].size(),s.size()-it-dic[i].size())) );
+        vector<int> &ac = res[all[dic[i]]];
+        int it = -1;
+        for (int j=0;j<ac.size();j++){
+            if (ac[j] >= p){
+                it = ac[j];
+                break;
+            }
+        }
+        if (it != -1) ans = max( ans , 1 + solve(it + dic[i].size()) );
     }
-    return cache[s] = ans;
+    return cache[p] = ans;
 }
 
 int main(){
-    string s,t;
-    while (cin >> s && s[0] != '#') dic.push_back(s);    
+    string t;
+    int i = 0;
+    while (cin >> s && s[0] != '#'){
+        dic.push_back(s);
+        all[s] = i++;   
+    }
     while (cin >> s && s[0] != '#'){
         while (s[s.size()-1] != '|') {
             cin >> t;
             s.append(t);
         }
         s = s.substr(0,s.size()-1);
-        // s is ready        
-        cout << solve(s) << endl;        
+        // s is ready
+        aho_corasick AC(dic);
+        cache.clear();
+        res = AC.search(s);  
+        cout << solve(0) << endl;        
     }
     
     return 0;
--- String Multimatching/a.cpp
+++ String Multimatching/a.cpp
@@ -1,62 +1,103 @@
 // Meysam Aghighi
-// Implementation of Rabin-Karp string multi-matching algorithm
+// Aho-Corasick's algorithm, find keywords' hits in a pattern
+using namespace std;
+#include <algorithm>
 #include <iostream>
 #include <cstring>
-#include <cmath>
+#include <cstdio>
 #include <vector>
-using namespace std;
+#include <queue>
+#include <map>
 
-typedef vector <vector<int> > vvi;
-#define d 256 // number of characters in the alphabet
+#define iter(it,c) for (__typeof((c).begin()) it = (c).begin(); it != (c).end(); ++it)
 
-// Rabin-Karp using hash values
-vvi RK(const vector<string> & pat, const string txt, int q){
-    int n = txt.size(), M = pat.size();
-    vector<int> m;
-    for (int i=0;i<M;i++) m[i] = pat[i].size(); // if error change to push_back()
-    // int t = 0; // hash values pat & txt
-    vector<int> p(M,0);
-    vector<int> t(M,0);
-    // int h = 1;
-    vector<int> h(M,1);
-    for (int j=0;j<M;j++)
-    for (int i=0;i<m[j]-1;i++) h[j] = (h[j] * d) % q;    
-    vvi ans;
- 
-    for (int j=0;j<M;j++)
-    for (int i=0;i<m[j];i++){
-        p[j] = (d*p[j] + pat[i])%q;
-        t[j] = (d*t[j] + txt[i])%q;
-    }
- 
-    for (int i=0;i<=n-m;i++){
-        if (p == t && txt.substr(i,m) == pat) ans.push_back(i);
-        if (i < n-m){
-            t = (d*(t - txt[i]*h) + txt[i+m]) % q;
-            if (t < 0) t += q;
+typedef vector<vector<int> > vvi;
+map<string,int> all;
+
+// Aho-Corasick from SuprDewd
+struct aho_corasick {
+    struct out_node {
+        string keyword; out_node *next;
+        out_node(string k, out_node *n) : keyword(k), next(n) { }
+    };
+    struct go_node {
+        map<char, go_node*> next;
+        out_node *out; go_node *fail;
+        go_node() { out = NULL; fail = NULL; }
+    };
+    go_node *go;
+    int N;
+    aho_corasick(vector<string> keywords) {
+        N = keywords.size();
+        go = new go_node();
+        iter(k, keywords) {
+            go_node *cur = go;
+            iter(c, *k)
+                cur = cur->next.find(*c) != cur->next.end() ? cur->next[*c] :
+                    (cur->next[*c] = new go_node());
+            cur->out = new out_node(*k, cur->out);
+        }
+        queue<go_node*> q;
+        iter(a, go->next) q.push(a->second);
+        while (!q.empty()) {
+            go_node *r = q.front(); q.pop();
+            iter(a, r->next) {
+                go_node *s = a->second;
+                q.push(s);
+                go_node *st = r->fail;
+                while (st && st->next.find(a->first) == st->next.end())
+                    st = st->fail;
+                if (!st) st = go;
+                s->fail = st->next[a->first];
+                if (s->fail) {
+                    if (!s->out) s->out = s->fail->out;
+                    else {
+                        out_node* out = s->out;
+                        while (out->next) out = out->next;
+                        out->next = s->fail->out;
+                    }
+                }
+            }
         }
     }
-    return ans;
-}
+    vvi search(string s){
+        vvi ress(N);
+        go_node *cur = go;
+        iter(c, s) {
+            while (cur && cur->next.find(*c) == cur->next.end())
+                cur = cur->fail;
+            if (!cur) cur = go;
+            cur = cur->next[*c];
+            if (!cur) cur = go;
+            for (out_node *out = cur->out; out; out = out->next){
+                ress[all[out->keyword]].push_back(c-s.begin()-out->keyword.size()+1);
+            }
+        }
+        return ress;
+    }
+};
 
 int main(){
-    string txt;
-    int x;
+    string text;
     int n;
     while (cin >> n){
-        vector<string> pat;
-        getline(cin,txt);
+        all.clear();
+        vector<string> keywords;
+        getline(cin,text);
         for (int i=0;i<n;i++){
-            getline(cin,txt);
-            pat.push_back(txt);
+            getline(cin,text);
+            keywords.push_back(text);
+            all[text] = i;
         }
-        getline(cin,txt);
-        vvi t = vvi(RK(pat,txt,101));
-        for (int j=0;j<t.size();j++){
-            for (int k=0;k<t[j].size();k++) cout << t[j][k] << " ";
+        getline(cin,text);
+        aho_corasick AC(keywords);
+        vvi res = AC.search(text);
+        for (int i=0;i<n;i++){
+            int ii = all[keywords[i]];
+            for (int j=0;j<res[ii].size();j++)
+                if (!j || (j && res[ii][j] != res[ii][j-1])) cout << res[ii][j] << " "; // if takes care of duplicates
             cout << endl;
-        }        
+        }
     }
-    
-    return 0;
+   return 0;
 }
