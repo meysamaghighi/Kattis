60c8e862bd8f998557453d1c8ed4fd150a073930
Meysam Aghighi <meyag26@lille62.ida.liu.se>
2016-06-27 17:01:35 +0200

    stuck in a time loop

---
+++ Stuck In A Time Loop/a.cpp
@@ -0,0 +1,10 @@
+// Meysam Aghighi
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i=0;i<n;i++) cout << i+1 << " Abracadabra" << endl; 
+    return 0;
+}
