f47f3da3c698b10cd0378aa60ca112a98cc42d1b
meysamaghighi <meysam.aghighi@gmail.com>
2016-08-19 13:31:28 +0200

    update

---
+++ Bijele/a.cpp
@@ -0,0 +1,11 @@
+// Meysam Aghighi
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << 1-a << " " << 1-b << " " << 2-c << " " << 2-d << " " << 2-e << " " << 8-f << endl;
+    
+    return 0;
+}
