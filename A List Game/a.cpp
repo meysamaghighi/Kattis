75f1fce5eaef70543e208a40aad7ac05d3585a0c
Meysam Aghighi <meysam.aghighi@gmail.com>
2017-12-02 11:02:01 +0100

    above average & a list game

---
+++ A List Game/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N, n, ans = 0, p = 2;
+    cin >> N; n = N;
+    while (n>1){
+        if (p*p>N){
+            ans++;
+            break;
+        }
+        while (n%p==0){
+            n /= p;
+            ans++;            
+        }
+        p++;        
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ Above Average/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int c,n;
+    cin >> c;
+    while (c--){
+        cin >> n;
+        int s = 0, a[n];
+        for (int i=0;i<n;i++) {
+            cin >> a[i];
+            s += a[i];
+        }
+        double avg = (s + 0.0)/n;
+        int m = 0; // how many are above average
+        for (int i=0;i<n;i++) if (a[i] > avg) m++;
+        double sol = (m+0.0)/n*100;
+        cout << fixed << setprecision(3) << sol << "%" << endl;
+    }
+    return 0;
+}
